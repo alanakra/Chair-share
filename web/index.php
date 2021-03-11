72a3f3e52c0a53e8c558dfe93bdb292c013b1f5e
Alan <alanakra@hotmail.fr>
2021-03-11 21:03:42 +0100

    Change file web extension

---
+++ web/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+ <title>Suivi de l'activité</title>
+</head>
+<body>
+ <h1 class="text-center">Suivi de l'activité</h1>
+
+ <div class="container">
+  <div class="row">
+
+   <table class="table">
+    <thead>
+     <tr>
+       <th scope="col">Date</th>
+       <th scope="col">Masse</th>
+     </tr>
+   </thead>
+   <tbody>
+    <tr>
+     <td></td>
+     <td></td>
+   </tr>
+   </tbody>
+   </table>
+   
+  </div>
+ </div>
+</body>
+</html>
